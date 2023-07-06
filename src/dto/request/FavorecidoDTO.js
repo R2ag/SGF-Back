3358a1306407efc574baadc38ab40d59f4f10785
Rafael <rlag@outlook.com>
2023-07-05 23:08:24 -0300

    Alteração no validador de favorecido DTO

--- src/dto/request/FavorecidoDTO.js
+++ src/dto/request/FavorecidoDTO.js
@@ -3,8 +3,8 @@ import * as yup from "yup";
 const favorecidoSchema = yup.object().shape({
     nome: yup.string().required("O nome do favorecido deve ser preenchido.").min(2, "O nome do favorecido deve ter no minimo 2 caracteres").max(50, "O nome do favorecido deve ter no máximo 50 caracteres."),
     ramo: yup.string().required("O ramo do favorecido deve ser preenchido.").min(2, "O Ramo do favorecido deve ter no minimo 2 caracteres").max(20, "O ramo do favorecido deve ter no máximo 20 caracteres."),
-    cpfOuCnpj: yup.string().optional().matches(/^(?:(\d{3})\.\d{3}\.\d{3}-\d{2}|\d{2}\.\d{3}\.\d{3}\/\d{4}-\d{2})$/),
-    email: yup.string().optional().email("Email invalido"),
+    cpfOuCnpj: yup.string().matches(/^(?:(\d{3})\.\d{3}\.\d{3}-\d{2}|\d{2}\.\d{3}\.\d{3}\/\d{4}-\d{2})?$/),
+    email: yup.string().email("Email invalido"),
 
 })
 
